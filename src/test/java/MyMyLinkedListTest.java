0aa61004f84fb082b49c102c16f6a26611235506
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-07 23:06:36 +0530

    implement add()

--- src/test/java/MyMyLinkedListTest.java
+++ src/test/java/MyMyLinkedListTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 public class MyMyLinkedListTest {
     @Test
     public void threeNumbersAfterAddShouldBeOnTop(){
-        MyNode<Integer> first=new MyNode<>(56);
-        MyNode<Integer> second=new MyNode<>(35);
-        MyNode<Integer> third=new MyNode<>(70);
+        MyNode<Integer> first=new MyNode<>(70);
+        MyNode<Integer> second=new MyNode<>(30);
+        MyNode<Integer> third=new MyNode<>(56);
         MyLinkedList<Integer> linkedList=new MyLinkedList<>();
         linkedList.add(first);
         linkedList.add(second);
         linkedList.add(third);
-        Assert.assertEquals(Integer.valueOf(56),linkedList.pop().getKey());
+        Assert.assertEquals(first,linkedList.pop());
     }
 }
