f350ebbd57f4fa9bb0c82f1cad55323784078676
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-08 23:03:37 +0530

    implement insert(), popLast()

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,37 +1,89 @@
 public class MyLinkedList<K> {
     public INode<K> head;
     public INode<K> tail;
+    private int n;
 
     public MyLinkedList() {
+        head = null;
+        tail = null;
+        n = 0;
     }
 
-    public void add(INode<K> newNode){
-        if(this.tail==null)
-            this.tail=newNode;
-        if(this.head==null)
-            this.head=newNode;
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(INode<K> newNode) {
+        if (this.tail == null)
+            this.tail = newNode;
+        if (this.head == null)
+            this.head = newNode;
         else {
-            INode<K> temp=this.head;
-            this.head=temp;
+            INode<K> temp = this.head;
+            this.head = newNode;
             head.setNext(temp);
         }
+        n++;
     }
 
-    public void append(INode<K> newNode){
-        if(this.tail==null)
-            this.tail=newNode;
-        if(this.head==null)
-            this.head=newNode;
+    public void append(INode<K> newNode) {
+        if (this.tail == null)
+            this.tail = newNode;
+        if (this.head == null)
+            this.head = newNode;
         else {
-            INode<K> temp=this.tail;
-            this.tail=temp;
-            tail.setNext(temp);
+            INode<K> temp = this.tail;
+            this.tail = newNode;
+            temp.setNext(tail);
+        }
+        n++;
+    }
+
+    public void insert(int pos, INode<K> item) {
+        if (pos < 0 || pos > size()) return;
+        if (isEmpty()) add(item);
+        else if (pos == 0) add(item);
+        else if (pos == size()) append(item);
+        else {
+            INode<K> current;
+            current = head;
+            for (int i = 1; i < pos; i++)
+                current = current.getNext();
+            item.setNext(current.getNext());
+            current.setNext(item);
+            n++;
         }
     }
 
-    public INode<K> pop(){
-        INode<K> temp=this.head;
-        this.head=temp.getNext();
+    public INode<K> popLast() {
+        INode<K> temp = head;
+        head = head.getNext();
+        //temp.setNext(null);
+        n--;
         return temp;
+    }
+
+    public INode<K> pop() {
+        INode<K> current = head;
+        for (int i = 0; i < n - 2; i++)
+            current = current.getNext();
+        INode<K> temp = tail;
+        current.setNext(null);
+        tail = current;
+        n--;
+        return temp;
+    }
+
+    public void printList() {
+        INode<K> current = head;
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(i + " " + current.getKey());
+            current = current.getNext();
+        }
     }
 }
--- src/test/java/MyMyLinkedListTest.java
+++ src/test/java/MyMyLinkedListTest.java
@@ -3,27 +3,62 @@ import org.junit.Test;
 
 public class MyMyLinkedListTest {
     @Test
-    public void threeNumbersAfterAddShouldBeOnTop(){
-        MyNode<Integer> first=new MyNode<>(70);
-        MyNode<Integer> second=new MyNode<>(30);
-        MyNode<Integer> third=new MyNode<>(56);
-        MyLinkedList<Integer> linkedList=new MyLinkedList<>();
+    public void threeNumbersAfterAddShouldBeOnTop() {
+        MyNode<Integer> first = new MyNode<>(70);
+        MyNode<Integer> second = new MyNode<>(30);
+        MyNode<Integer> third = new MyNode<>(56);
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
         linkedList.add(first);
         linkedList.add(second);
         linkedList.add(third);
-        Assert.assertEquals(first,linkedList.pop());
+        linkedList.printList();
+        Assert.assertEquals(first, linkedList.pop());
+        Assert.assertEquals(second, linkedList.pop());
+        Assert.assertEquals(third, linkedList.pop());
+
+
     }
 
     @Test
-    public void threeNumbersAfterAppendShouldBeOnTop(){
-        MyNode<Integer> first=new MyNode<>(56);
-        MyNode<Integer> second=new MyNode<>(30);
-        MyNode<Integer> third=new MyNode<>(70);
-        MyLinkedList<Integer> linkedList=new MyLinkedList<>();
+    public void threeNumbersAfterAppendShouldBeOnTop() {
+        MyNode<Integer> first = new MyNode<>(56);
+        MyNode<Integer> second = new MyNode<>(30);
+        MyNode<Integer> third = new MyNode<>(70);
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
         linkedList.append(first);
         linkedList.append(second);
         linkedList.append(third);
-        Assert.assertEquals(first,linkedList.pop());
+        linkedList.printList();
+        Assert.assertEquals(first, linkedList.popLast());
+        Assert.assertEquals(second, linkedList.popLast());
+        Assert.assertEquals(third, linkedList.popLast());
+    }
 
+    @Test
+    public void threeNumbersInsertTest() {
+        MyNode<Integer> first = new MyNode<>(56);
+        MyNode<Integer> second = new MyNode<>(30);
+        MyNode<Integer> third = new MyNode<>(70);
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        linkedList.append(first);
+        linkedList.append(third);
+        linkedList.insert(1, second);
+        Assert.assertEquals(third, linkedList.pop());
+        Assert.assertEquals(second, linkedList.pop());
+        Assert.assertEquals(first, linkedList.pop());
+
+    }
+
+    @Test
+    public void deleteLastTest() {
+        MyNode<Integer> first = new MyNode<>(56);
+        MyNode<Integer> second = new MyNode<>(30);
+        MyNode<Integer> third = new MyNode<>(70);
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        linkedList.add(first);
+        linkedList.add(second);
+        linkedList.add(third);
+        linkedList.popLast();
+        Assert.assertEquals(second, linkedList.popLast());
     }
 }
