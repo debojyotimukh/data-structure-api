aed2cc4b58d867b9672feba52cef32eb1a1f50a3
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-10 01:50:55 +0530

    implement: MyBinaryNode

---
+++ src/main/java/MyBinaryNode.java
@@ -0,0 +1,12 @@
+public class MyBinaryNode<K extends Comparable<K>> {
+    K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.right=null;
+        this.left=null;
+    }
+
+}
