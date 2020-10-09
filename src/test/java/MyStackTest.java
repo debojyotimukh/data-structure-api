1c80ff8487d53604b2f82d4737a99d176fe2de1e
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-09 20:35:26 +0530

    formatting: MyStackTest.java

--- src/test/java/MyStackTest.java
+++ src/test/java/MyStackTest.java
@@ -3,16 +3,16 @@ import org.junit.Test;
 
 public class MyStackTest {
     @Test
-    public void stackPushPopTest(){
-        MyNode<Integer> first=new MyNode<>(70);
-        MyNode<Integer> second=new MyNode<>(30);
-        MyNode<Integer> third=new MyNode<>(56);
-        MyStack<Integer> stack=new MyStack<>();
+    public void stackPushPopTest() {
+        MyNode<Integer> first = new MyNode<>(70);
+        MyNode<Integer> second = new MyNode<>(30);
+        MyNode<Integer> third = new MyNode<>(56);
+        MyStack<Integer> stack = new MyStack<>();
         stack.push(first);
         stack.push(second);
         stack.push(third);
-        Assert.assertEquals(third,stack.pop());
-        Assert.assertEquals(second,stack.pop());
-        Assert.assertEquals(first,stack.pop());
+        Assert.assertEquals(third, stack.pop());
+        Assert.assertEquals(second, stack.pop());
+        Assert.assertEquals(first, stack.pop());
     }
 }
